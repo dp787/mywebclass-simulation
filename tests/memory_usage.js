ea35f4f407e8814e4cdc34f415bbbe6733d3a498
cr432 <114003161+cr432@users.noreply.github.com>
2023-03-27 12:29:17 -0400

    memory usage test

---
+++ tests/memory_usage.js
@@ -0,0 +1,21 @@
+const { chromium } = require('playwright');
+
+describe('Website memory usage', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await chromium.launch();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+  });
+
+  afterEach(async () => {
+    await page.close();
+  });
